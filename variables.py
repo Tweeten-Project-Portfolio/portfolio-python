9168b9092ec1ee6c31fa60d286f6e5b9c1821b45
Tyler Tweeten <tweeten.tyler@gmail.com>
2025-03-11 14:30:27 -0500

    Update variables.py
    Added helper functions

--- variables.py
+++ variables.py
@@ -1,13 +1,31 @@
 # Demonstrating my knowledge of how variables work and basic variable functions
 
-price = float(input("Please type in the price of the item"))
-quantity = int(input("Please type in how many items were purchased"))
-tax_rate = float(input("Please type in the effective tax rate"))
-discount = float(input("Please type in the discount on this item, if applicable"))
+# Defining some helper functions to handle input better
+
+def get_float_input(prompt):
+  while True:
+    try:
+      return float(input(prompt))
+    except ValueError:
+      print("Unexpected input. Please enter a numeric value.")
+
+def get_int_input(prompt):
+  while True:
+    try:
+      return int(input(prompt))
+    except ValueError:
+      print("Unexpected input. Please enter a whole number.")
+
+# Defining calc functions
+
+price = get_float_input("Please type in the price of the item ")
+quantity = get_int_input("Please type in how many items were purchased ")
+tax_rate = get_float_input("Please type in the effective tax rate ")
+discount = get_float_input("Please type in the discount on this item, if applicable ")
 
 total_before_tax = price*quantity
 discounted_price = total_before_tax - (total_before_tax*(discount/100))
 tax_amount = discounted_price*(tax_rate/100)
 final_price = discounted_price+tax_amount
 
-print(f"Your total is {final_price}")
+print(f"Your total is {final_price:.2f}")
