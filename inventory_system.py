9855a4aaced6fe034c701429191e882afe3eef19
Tyler Tweeten <tweeten.tyler@gmail.com>
2025-03-11 15:23:21 -0500

    Update inventory_system.py
    Updated error handling and logic

--- inventory_system.py
+++ inventory_system.py
@@ -1,16 +1,40 @@
 # Mock build of an inventory system for a store that tracks products and quantities
 
+# define error handling
+
+def get_input(prompt):
+  while True:
+    try:
+      return (input(prompt))
+    except ValueError:
+      print("Unexpected value. Please input a non-numeric value and try again.")
+
+def get_int_input(prompt):
+  while True:
+    try:
+      return int(input(prompt))
+    except ValueError:
+      print("Unexpected value. Please input a whole number and try again.")
+
+def get_float_input(prompt):
+  while True:
+    try:
+      return float(input(prompt))
+    except ValueError:
+      print("Unexpected value. Please input a numeric value and try again.")
+
+
 # Define blank array to store data types in
 inventory = []
 
-item = int(input("Input the number of unique items: "))
+num_item = get_int_input("Input the number of unique items: ")
 
-# For loop for # of items input
-for _ in range(item):
-  name = (input("Product name? "))
-  price = float(input("Product price? "))
-  quantity = int(input("Product quantity? "))
-  tags = (input("Product type? "))
+# For loop for # of num_items input
+for _ in range(num_item):
+  name = get_input("Product name? ")
+  price =get_float_input("Product price? ")
+  quantity = get_int_input("Product quantity? ")
+  tags =get_input("Product type? ").split(",")
 
   inventory.append({
     "name": name,
@@ -27,9 +51,9 @@ discount_percent = discount/100
 def calculate_inventory_value(inventory,discount):
   total_value = 0
 
-  for item in inventory:
-    discounted_price = item["price"]*(1 - (discount/100))
-    total_value += discounted_price*item["quantity"]
+  for num_item in inventory:
+    discounted_price = num_item["price"]*(1 - (discount/100))
+    total_value += discounted_price*num_item["quantity"]
 
   return total_value
 
