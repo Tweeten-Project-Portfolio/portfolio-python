9ddcd1690f0b863ef96b9cba791664c6a523963c
Tyler Tweeten <tweeten.tyler@gmail.com>
2025-03-11 22:43:57 -0500

    Update inventory_system.py
    Updated code commentary

--- inventory_system.py
+++ inventory_system.py
@@ -1,6 +1,6 @@
 # Mock build of an inventory system for a store that tracks products and quantities
 
-# define error handling
+# Define error handling using a while loop. Since we don't know how many times it will take for the user to get the right input, we use "while True:"
 
 def get_input(prompt):
   while True:
@@ -27,6 +27,7 @@ def get_float_input(prompt):
 # Define blank array to store data types in
 inventory = []
 
+# Define the number of items needing defined in the array
 num_item = get_int_input("Input the number of unique items: ")
 
 # For loop for # of num_items input
@@ -34,8 +35,9 @@ for _ in range(num_item):
   name = get_input("Product name? ")
   price =get_float_input("Product price? ")
   quantity = get_int_input("Product quantity? ")
-  tags =get_input("Product type? ").split(",")
+  tags =get_input("Product type? ").split(",") # Option for multiple product catagories
 
+  # Adding each item to the array
   inventory.append({
     "name": name,
     "price": price,
@@ -47,7 +49,7 @@ for _ in range(num_item):
 discount = float(input("Enter the discount percentage: "))
 discount_percent = discount/100
 
-# Define inventory calculation function
+# Define inventory calculation function. We use a loop to store values and add as we go through the math for each item.
 def calculate_inventory_value(inventory,discount):
   total_value = 0
 
@@ -57,7 +59,8 @@ def calculate_inventory_value(inventory,discount):
 
   return total_value
 
-# Final function calculation
+# Final function calculation. Calling the function above with it's calculated output
 final_inventory_value = calculate_inventory_value(inventory, discount)
 
+# Printing final value using .2f to denote 2 decimal places
 print(f"The final inventory value after discounts is {final_inventory_value:.2f}")
