921e699c3e26cf5f6228e0505e739cc94f44cf5d
Tyler Tweeten <tweeten.tyler@gmail.com>
2025-03-11 23:30:29 -0500

    Update control_flow.py
    Updates to get base array loop working right

--- control_flow.py
+++ control_flow.py
@@ -3,20 +3,28 @@
 def get_int_input(prompt):
   while True:
     try:
-      return(input(prompt))
+      return(int(input(prompt)))
     except ValueError:
       print("Unexpected input. Please input a whole number and try again.")
 
 # Defining a blank array
 numbers = []
 
-count = get_int_input("How many numbers would you like to enter?")
+total = 0
 
-for _ in range(count):
-  value = get_int_input("What number would you like to analyze?")
-  number.append({
-    "value": value,
-  })
+count = get_int_input("How many numbers would you like to enter? ")
+
+while total<count:
+  value = get_int_input("Which number would you like to analyze? ")
+
+  try:
+    num = (value)
+    numbers.append({
+      num
+    })
+    total+=1
+
+  except ValueError:
+    print("Unexpected input, try again.")
 
 print(numbers)
-  
